717113f00596007bd7ba651c0a7dd22d663af6a7
Wescley Silva <73146483+WescleySil@users.noreply.github.com>
2024-09-07 22:35:40 +0000

    [FIX] :hammer: Adding database to index.js to create database when start application

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 8000;
 const customerRoutes = require('./routes/customer')
+const db = require('./database');
 
 app.use(bodyParser.json());
 
